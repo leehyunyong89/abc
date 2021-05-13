99565df1f8524e4cef516958e3209a77df2d43b3
leehyunyong89 <74758862+leehyunyong89@users.noreply.github.com>
2021-05-13 11:59:17 +0900

    네 번째 수정
    hi,hi-2,hi-3 삭제했음

--- Hellojava.java
+++ Hellojava.java
@@ -3,9 +3,7 @@ public class Hellojava {
 
 	public static void main(String[] args) {
 		System.out.println("Hello");
-		System.out.println("Hi");
-		System.out.println("Hi-2");
-		System.out.println("Hi-3");
+		
 	}
 
 }
