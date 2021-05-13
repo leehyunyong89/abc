fdc9a16280b0fe7e05c3709e36cb9df2841cab68
leehyunyong89 <74758862+leehyunyong89@users.noreply.github.com>
2021-05-13 11:55:59 +0900

    세 번째 수정
    Hi-2 , Hi-3 추가했음

--- Hellojava.java
+++ Hellojava.java
@@ -4,7 +4,8 @@ public class Hellojava {
 	public static void main(String[] args) {
 		System.out.println("Hello");
 		System.out.println("Hi");
-
+		System.out.println("Hi-2");
+		System.out.println("Hi-3");
 	}
 
 }
